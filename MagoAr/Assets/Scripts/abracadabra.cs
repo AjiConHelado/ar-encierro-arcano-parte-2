4f20b822bc13f3828ad24d69c87e44f1432bf506
AjiConHelado <79388740+AjiConHelado@users.noreply.github.com>
2023-08-25 20:43:35 +0200

    si

---
+++ MagoAr/Assets/Scripts/abracadabra.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class abracadabra : MonoBehaviour
+{
+    public GameObject planefinder;
+
+    public void Onencontrarcosa()
+    {
+        planefinder.SetActive(false);
+    }
+
+}
